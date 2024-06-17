8cc3eb50e078b0c9e31dbfc6a6c463b8f361d457
John Carr <john.carr@unrouted.co.uk>
2024-06-17 11:25:07 +0100

    fix: added more typing information

--- aiojellyfin/__init__.py
+++ aiojellyfin/__init__.py
@@ -16,6 +16,32 @@ DEFAULT_FIELDS: Final[str] = (
 )
 
 
+class MediaStream(TypedDict, total=False):
+    """Information abouta Jellyfin stream."""
+
+    Channels: int
+    Codec: str
+
+
+class MediaSource(TypedDict, total=False):
+    """Information about a Jellyfin media source."""
+
+    Path: str
+
+
+class ArtistItem(TypedDict):
+    """Information about a relationship between media and an artist."""
+
+    Id: str
+    Name: str
+
+
+class UserData(TypedDict, total=False):
+    """Metadata that is specific to the logged in user, like favorites."""
+
+    IsFavorite: bool
+
+
 class MediaLibrary(TypedDict, total=False):
     """JSON data describing a single media library."""
 
@@ -32,7 +58,40 @@ class MediaLibraries(TypedDict):
     StartIndex: int
 
 
-class Artist(TypedDict, total=False):
+class MediaItem(TypedDict, total=False):
+    """JSON data describing a single media item."""
+
+    Id: Required[str]
+    Name: str
+    MediaType: str
+    IndexNumber: int
+    SortName: str
+    AlbumArtist: str
+    Overview: str
+    ProductionYear: int
+    ProviderIds: dict[str, str]
+    CanDownload: bool
+    RunTimeTicks: int
+    MediaStreams: list[MediaStream]
+    AlbumId: str
+    Album: str
+    ParentIndexNumber: int
+    ArtistItems: list[ArtistItem]
+    ImageTags: dict[str, str]
+    UserData: UserData
+    AlbumArtists: list[ArtistItem]
+    MediaSources: list[MediaSource]
+
+
+class MediaItems(TypedDict):
+    """JSON data describing a collection of media items."""
+
+    Items: list[MediaItem]
+    TotalRecordCount: int
+    StartIndex: int
+
+
+class Artist(MediaItem, total=False):
     """JSON data describing a single artist."""
 
     Id: Required[str]
@@ -43,20 +102,6 @@ class Artists(TypedDict):
     """JSON data describing a collection of artists."""
 
     Items: list[Artist]
-    TotalRecordCount: int
-    StartIndex: int
-
-
-class MediaItem(TypedDict, total=False):
-    """JSON data describing a single media item."""
-
-    Id: Required[str]
-
-
-class MediaItems(TypedDict):
-    """JSON data describing a collection of media items."""
-
-    Items: list[MediaItem]
     TotalRecordCount: int
     StartIndex: int
 
